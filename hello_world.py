b7a2a88c1707002cc3d2a5315bfa9dee2c7f9f86
pretty7in <pretty7in@gmail.com>
2024-01-09 10:26:24 +1100

    Py from start

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+print("Hello World")
+print("hello Python World")
+message = "Hello Python World"
+print(message)
+message = "hello crash course world"
+print(message)
+name = "priyanka bhatnagar"
+print(name.title())
+print(name.upper())
+first_name = "priyanka"
+last_name = "bhatnagar"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print(f"Hello, {full_name.title()} !")
+print("\tPython")
+print("")
